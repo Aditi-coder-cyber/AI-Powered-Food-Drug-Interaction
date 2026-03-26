1e85bed71e1f3521a8f6118e0e7c0f50290deb4e
Amber <gargamber03@gmail.com>
2026-03-26 10:08:49 +0530

    Smart Nutrition Analyzer

---
+++ server/feature/nutritionAnalyzer/nutrition.service.js
@@ -0,0 +1,9 @@
+// pasted & cleaned logic from Smart-Nutrition-Analyzer
+
+function analyzeNutrition(data) {
+
+   // your analyzer logic
+   return result;
+}
+
+module.exports = { analyzeNutrition };
